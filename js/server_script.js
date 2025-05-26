130d878bfe2d80c793ec9a467d36616699db0ad7
Eto12 <reverso12yt@gmail.com>
2025-05-25 21:25:51 -0300

    a

--- js/server_script.js
+++ js/server_script.js
@@ -1,4 +1,4 @@
-const webservice = "https://imenu-backend-yp5c.onrender.com";
+const webservice = "https://imenu-backend-yp5c.onrender.com" //"http://localhost:3006";
 
 // Criar usuário
 async function criaruser() {
