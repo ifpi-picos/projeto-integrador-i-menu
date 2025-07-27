a41c814962afa9e693dee019798b7934ce9ffa1c
Fernandaellen002 <fernandaellensousas@gmail.com>
2025-07-27 16:58:23 -0300

    configuraçoes

--- js/pesquisa.js
+++ js/pesquisa.js
@@ -1,6 +1,6 @@
 // Select relevant HTML elements
 const filterButtons = document.querySelectorAll("#filter-buttons button");
-const filterableCards = document.querySelectorAll("#filterable-cards .card");
+const filterableCards = document.querySelectorAll(" .card");
 
 // Function to filter cards based on filter buttons
 const filterCards = (e) => {
